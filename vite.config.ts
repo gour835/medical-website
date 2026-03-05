2b9ed5c0c410a91c8457a0cea286cab6eca7c18b
Gouransh <gouranshnayak1234@gmail.com>
2026-03-05 15:35:29 +0530

    vercel configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
     plugins: [
@@ -11,17 +11,23 @@ export default defineConfig({
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
+
         react({
             babel: {
                 plugins: ['babel-plugin-react-compiler'],
             },
         }),
+
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
+
+        process.env.NODE_ENV === 'development'
+            ? wayfinder({
+                formVariants: true,
+            })
+            : null,
+    ].filter(Boolean),
+
     esbuild: {
         jsx: 'automatic',
     },
-});
+})
